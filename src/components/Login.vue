299f02a2f3c770fbcbb1cddcb8fcbda1986c2120
christine <nessymungla5@gmail.com>
2023-09-26 19:59:12 +0300

    Second commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,5 +3,16 @@
 <template>
     <div id="login">
         <h1>Log in to UniApp</h1>
+        <form class="form">
+       <div class="header1"> <h2>Sign In</h2></div>     
+        <input type="text" name="Username" class= "box" placeholder="Username" required>
+        <input type="password" name="password" class= "box" placeholder="password" required>
+        <input type="checkbox"><label form="rememberme">Remember me</label>
+        <input type="button" name="Submit" value="Submit" id="Submit">
+        
+        <center>
+        <div><p>Don't have an account? <br> <a href="signup.html" class="signup">Register here</a></p></div>
+        </center>
+    </form>
     </div>
 </template>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,5 +3,21 @@
 <template>
     <div id="signup">
         <h1>Sign Up to UniApp</h1>
+        <form class="form">
+       <div class="header1"> <h2>Sign up</h2></div>     
+        <input type="text" name="First Name" class= "box" placeholder="First Name" required>
+        <input type="text" name="Last Name" class= "box" placeholder="Last Name" required>
+        <input type="text" name="Other Name" class= "box" placeholder="Other Name" required>
+        <input type="date" name="DOB" class= "box" placeholder="DOB" requiredDOB>
+        <input type="email" name="email" class= "box" placeholder="email" required>
+        <input type="tel" name="phone Number" class= "box" placeholder="Phone Number" required>
+        <input type="text" name="Course" class= "box" placeholder="Course" required>
+        <input type="month" name="Intake" class= "box" placeholder="Intake" required>
+        <input type="button" name="Submit" value="Submit" id="Submit">
+        
+        <center>
+        <div><p> Have an account? <br> <a href="login.html" class="login">login here</a></p></div>
+        </center>
+    </form>
     </div>
 </template>
